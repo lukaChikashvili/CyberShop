670e320fa0a74dfdac9dc5aeaf4263cd8f036744
luka chikashvili <lukachikashvili2020@gmail.com>
2024-09-12 20:20:10 +0400

    add hero section title and paragraph

--- bank/src/App.jsx
+++ bank/src/App.jsx
@@ -6,8 +6,15 @@ function App() {
   return (
     <>
       <div className="frame">
-          <div className="inner_frame">
-           
+          <div className="inner_frame flex flex-col gap-8 items-center justify-center">
+              <h1 className="text-8xl text-white font-bold">CyberShop</h1>
+              <p className="text-white text-xl">
+                 Discover the Future of Fashion and Tech
+              </p>
+
+              <p className="text-[#FFEB00] ">
+                 Shop Smarter. Shop Faster. Shop the Future.
+              </p>
          </div>
         </div>    
     </>
