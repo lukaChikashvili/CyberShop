e824a5df91d2ca3de2173866fa8ff53104f2ca69
luka chikashvili <lukachikashvili2020@gmail.com>
2024-09-13 14:33:34 +0400

    add react-router-dom

--- bank/src/App.jsx
+++ bank/src/App.jsx
@@ -1,27 +1,16 @@
-import { Info, ShoppingCart} from 'lucide-react'
+import { Route, Routes } from "react-router-dom"
+import Home from "./components/Home"
+
+
 
 function App() {
 
 
   return (
     <>
-      <div className="frame">
-          <div className="inner_frame flex flex-col gap-8 items-center justify-center">
-              <h1 className="text-8xl text-white font-bold">CyberShop</h1>
-              <p className="text-white text-xl">
-                 Discover the Future of Fashion and Tech
-              </p>
-
-              <p className="text-[#FFEB00] ">
-                 Shop Smarter. Shop Faster. Shop the Future.
-              </p>
-
-          <div className="flex items-center gap-8">
-              <button className="futuristic_button flex items-center gap-4"><ShoppingCart />Start buying</button>
-              <button className="futuristic_button flex items-center gap-4"><Info />About us</button>
-              </div>
-         </div>
-        </div>    
+    <Routes>
+       <Route path="/" element = {<Home />} />
+    </Routes>
     </>
   )
 }
