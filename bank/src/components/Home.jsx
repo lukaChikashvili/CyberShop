769490d53871df85243e9fcd83b8d709d9d7f010
luka chikashvili <lukachikashvili2020@gmail.com>
2024-09-15 18:54:59 +0400

    add responsive design on home page

--- bank/src/components/Home.jsx
+++ bank/src/components/Home.jsx
@@ -16,18 +16,18 @@ const Home = () => {
   return (
     <div className="frame">
           <div className="inner_frame flex flex-col gap-8 items-center justify-center">
-              <h1 className="text-8xl text-white font-bold">CyberShop</h1>
-              <p className="text-white text-xl">
+              <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl text-white font-bold">CyberShop</h1>
+              <p className="text-white sm:text-lg md:text-xl lg:text-2xl text-center">
                  Discover the Future of Fashion and Tech
               </p>
 
-              <p className="text-[#FFEB00] ">
+              <p className="text-[#FFEB00] text-sm sm:text-base md:text-lg lg:text-xl ">
                  Shop Smarter. Shop Faster. Shop the Future.
               </p>
 
-          <div className="flex items-center gap-8">
-              <button className="futuristic_button flex items-center gap-4" onClick={showProducts}><ShoppingCart />Start buying</button>
-              <button className="futuristic_button flex items-center gap-4"><Info />About us</button>
+          <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 md:gap-8 items-center justify-center">
+              <button className="futuristic_button  flex items-center justify-center gap-2 sm:gap-4  text-sm sm:text-base" onClick={showProducts}><ShoppingCart />Start buying</button>
+              <button className="futuristic_button flex items-center justify-center gap-2 sm:gap-4 text-sm sm:text-base"><Info />About us</button>
               </div>
          </div>
 
